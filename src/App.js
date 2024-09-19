8abdfc16671a26a21ba1164c302a97e00a812bd8
kimgaaaa <dl4525@naver.com>
2024-09-19 19:20:27 +0900

    axios 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react';
+import bg from './img/img1.jpg';
+import data from './data.js';
+import { Route, Routes, Link, Outlet} from 'react-router-dom';
+import Detail from './routes/Detail.js';
+import Detail1 from './routes/Detail1.js';
+import axios from 'axios';
 
 function App() {
+  let [datas] = useState(data)
+  const [posts, setPosts]=useState([]);
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      console.log(response.data)
+      setPosts(response.data)
+    });
+
+    /* axios({
+      method:'Get',
+      url: 'https://jsonplaceholder.typicode.com/users'
+    }).then(response => {
+      console.log(response.data)
+      setPosts(response.data)
+    }) */
+
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <nav className='nav'>
+              <h1 className="logo"><Link to="/">Blog</Link></h1>
+              <ul className="menu">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/detail1">상세페이지</Link></li>
+                <li><Link to="/about">Best</Link></li>
+              </ul>
+            </nav>
+            <div className="main-bg" style={{backgroundImage : 'url('+bg+')'}}></div>
+
+            <container className="mt20">
+              <h2 className="mb20">Best of Best</h2>
+              <ul className='fb'>
+                {/* <Card datas={datas[0]} i={1} />
+                <Card datas={datas[1]} i={2} />
+                <Card datas={datas[2]} i={3} /> */}
+                {
+                  datas.map((item, i)=>{
+                    return (
+                      <Card datas={datas[i]} i={i+1} key={i}/>
+                    )
+                  })
+                }
+              </ul>
+            </container>
+            <br />
+            <ul className='user'>
+              {posts.map(post => (
+                <li key={post.id}>
+                  <div>{post.name}</div>
+                  <div>{post.email}</div>
+                  <div>{post.address.city}</div>
+
+                </li>
+              ))}
+            </ul>
+        </div>}/>
+        <Route path="/detail1" element={<Detail1 />}/>
+        <Route path="/detail/:id" element={<Detail datas={datas} />}/>
+        <Route path="/about" element={<About />}> 
+                <Route path="member" element={<div>멤버임</div>}></Route>
+                <Route path="location" element={<About />}></Route>
+        </Route>
+      </Routes>
+      
+          
     </div>
+ 
+    
   );
+}
+
+function About(){
+  return(
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+
+function Card(props){
+  return(
+    <li >
+      <Link to={`/detail/${props.datas.id}`}>
+        <img src={props.datas.imges} width="80%" alt={props.datas.title} />
+        <h4 className="mt20">{props.datas.title}</h4>
+        <p>{props.datas.content}</p>
+        <p className='price'>{props.datas.price}</p>
+      </Link>
+    </li>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,25 @@
+let data=[
+    {
+        id: 0,
+        title:'내가 좋아하는 조명',
+        content:'은은한 조명에 잠이 솔솔 와요',
+        price: 25000,
+        imges:'/img/img1.jpg'
+    },
+    {
+        id: 1,
+        title:'내가 좋아하는 의자',
+        content:'인테리어 소품으로 아주 좋아요',
+        price: 37000,
+        imges:'/img/img2.jpg'
+    },
+    {
+        id: 2,
+        title:'내가 좋아하는 거울',
+        content:'깨끗하고 말끔해지는 거울',
+        price: 58000,
+        imges:'/img/img3.jpg'
+    }
+]
+
+export default data;
